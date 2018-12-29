47635d2b8151e0be00cd9a08d4785d980191ca21
Kyle <35588499+SDBagel@users.noreply.github.com>
2018-12-29 00:18:47 -0800

    Minor layout changes
    Positioning and spacing of top header and footer
    Resized trackr.svg
    Added content example for Trackr
    Reworked site.js

--- static/js/site.js
+++ static/js/site.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
-  $("#title-icon").fadeTo(600, 1).promise().done(function () {
-    $("#title").fadeTo(800, 1).promise().done(function () {
-      $("#title-caption").fadeTo(600, 1).promise().done(function () {
-        $("#scroll-icon").animate({'opacity':'1', 'margin-top':'42vh'}, 600);
-      });
+  $("#group-title").fadeTo(800, 1).promise().done(function() {
+    $("#navbar").fadeTo(800, 1).promise().done(function() {
+      $("#trackr").fadeTo(800, 1);
     });
   });
 });
