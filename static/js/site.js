7c570cc3262a74ffe1426768fc997a557ff197fe
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-07-03 20:10:27 -0700

    Functional buttons & minification
    Compacted bits and stuff so the thing gets called less and that's better. yeaaaaaaaaah boi

--- static/js/site.js
+++ static/js/site.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   
 });
+
+function scrollToProducts() {
+    var el = document.getElementById("products");
+    var top = el.getBoundingClientRect().top;
+
+    scroll(0, top - 40);
+}
