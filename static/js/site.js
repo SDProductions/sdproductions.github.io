173be657c7c89103c925af42388402981e3a2067
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-07-03 23:20:51 -0700

    About section

--- static/js/site.js
+++ static/js/site.js
@@ -1,7 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  
-});
-
 function scrollToProducts() {
     var el = document.getElementById("products");
     var top = el.getBoundingClientRect().top;
